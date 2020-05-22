44feebb1595b6c33b4f6dd3b347da129455ded69
mengfanxing <479922976@qq.com>
2020-05-22 16:21:09 +0800

    router的改进

---
+++ controller/user.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func LoadUser(c *gin.Context){
+	c.JSON(http.StatusOK,gin.H{
+		"message":"load user success",
+	})
+}
+
+func CreateUser(c *gin.Context){
+	c.JSON(http.StatusOK,gin.H{
+		"message":"create user success",
+	})
+}
